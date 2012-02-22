77edc53c320133b1b27f3f866206971f8acfb465
Jared Hance <jaredhance@gmail.com>
2012-02-22 15:41:45 -0500

    Function types

--- LLVM/Untyped/Core.hs
+++ LLVM/Untyped/Core.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable #-}
 module LLVM.Untyped.Core
     (
-    -- * Core Monad
-    LLVM,
+    -- * Core Monad LLVM,
 
     -- * Modules
     Module,
@@ -36,14 +35,22 @@ module LLVM.Untyped.Core
     doubleType,
     x86FP80Type,
     fp128Type,
-    ppcFP128Type
+    ppcFP128Type,
+
+    -- * Function Types
+    functionType,
+    isFunctionVarArg,
+    getReturnType,
+    countParamTypes,
+    getParamTypes
     )
 where
 
 import Control.Applicative
 import Data.Typeable
-import Foreign
+import Foreign hiding (unsafePerformIO)
 import Foreign.C.String
+import System.IO.Unsafe (unsafePerformIO)
 import qualified LLVM.FFI.Core as L
 
 newtype LLVM a = LLVM (IO a)
@@ -124,3 +131,43 @@ fp128Type = Type L.fp128Type
 
 ppcFP128Type :: Type
 ppcFP128Type = Type L.ppcFP128Type
+
+-- | We require IO to allocate an array. This is abstracted out with
+-- unsafePerformIO.
+functionType :: Type -- ^ Return Type
+                -> [Type] -- ^ Argument Types
+                -> Bool -- ^ Is Var Arg?
+                -> Type
+functionType (Type retType) argTypes isVarArg = unsafePerformIO $ do
+    let untype (Type t) = t
+        argTypes' = map untype argTypes
+        argCount = fromIntegral . length $ argTypes'
+        isVarArg' = fromIntegral $ case isVarArg of
+            True -> 1 -- Is this right?
+            False -> 0
+    resultRef <- withArray argTypes' $ \argTypesPtr -> return $
+        L.functionType retType argTypesPtr argCount isVarArg'
+    return $ Type resultRef
+
+-- Theoretically a lot of these don't need to be in the LLVM monad.
+-- Should experiment.
+
+isFunctionVarArg :: Type -> LLVM Int
+isFunctionVarArg (Type typeRef) = LLVM $ fromIntegral <$> L.isFunctionVarArg typeRef
+
+getReturnType :: Type -> LLVM Type
+getReturnType (Type typeRef) = LLVM $ Type <$> L.getReturnType typeRef
+
+countParamTypes :: Type -> LLVM Int
+countParamTypes (Type typeRef) = LLVM $ fromIntegral <$> L.countParamTypes typeRef
+
+getParamTypes :: Type -> LLVM [Type]
+getParamTypes functionType'@(Type functionType) = 
+    do numParams <- countParamTypes functionType'
+       LLVM $ do
+        typeArray <- mallocArray numParams
+        L.getParamTypes functionType typeArray
+        typeList <- do list <- peekArray numParams typeArray
+                       free typeArray
+                       return list
+        return $ map Type typeList
