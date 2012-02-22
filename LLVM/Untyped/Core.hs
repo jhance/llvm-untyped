b7f456bb21bbe81622c0e448778f04ba7b052d03
Jared Hance <jaredhance@gmail.com>
2012-02-22 15:13:31 -0500

    Fixes and more functions

--- LLVM/Untyped/Core.hs
+++ LLVM/Untyped/Core.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable #-}
 module LLVM.Untyped.Core
     (
     -- * Core Monad
@@ -18,7 +18,18 @@ module LLVM.Untyped.Core
 
     -- * Types
     Type,
-    addTypeName
+    L.TypeKind,
+    addTypeName,
+    deleteTypeName,
+    getTypeKind,
+    
+    -- * Integer Types
+    int1Type,
+    int8Type,
+    int16Type,
+    int32Type,
+    integerType,
+    getIntTypeWidth,
     )
 where
 
@@ -29,7 +40,7 @@ import Foreign.C.String
 import qualified LLVM.FFI.Core as L
 
 newtype LLVM a = LLVM (IO a)
-    deriving Monad
+    deriving (Applicative, Functor, Monad)
 
 newtype Module = Module L.ModuleRef
 
@@ -43,30 +54,51 @@ moduleCreateWithName :: String -- ^ Name of module
                         -> LLVM Module
 moduleCreateWithName name = LLVM $ Module <$> withCString name L.moduleCreateWithName
 
--- TODO
-disposeModule = undefined
-ptrDisposeModule = undefined
+disposeModule :: Module -> LLVM ()
+disposeModule (Module moduleRef) = LLVM $ L.disposeModule moduleRef
                                       
 getDataLayout :: Module -> LLVM String
 getDataLayout (Module moduleRef) = LLVM $ L.getDataLayout moduleRef >>= peekCAString
 
 setDataLayout :: Module -> String -> LLVM ()
-setDataLayout (Module moduleRef) layout = LLVM $ withCString layout L.setDataLayout moduleRef
+setDataLayout (Module moduleRef) layout = LLVM $ withCString layout (L.setDataLayout moduleRef)
 
 getTarget :: Module -> LLVM String
 getTarget (Module moduleRef) = LLVM $ L.getTarget moduleRef >>= peekCAString
 
 setTarget :: Module -> String -> LLVM ()
-setTarget (Module moduleRef) target = LLVM $ withCString target L.setTarget moduleRef
+setTarget (Module moduleRef) target = LLVM $ withCString target (L.setTarget moduleRef)
 
 createModuleProviderForExistingModule :: Module -> LLVM ModuleProvider
 createModuleProviderForExistingModule (Module moduleRef) =
-    ModuleProvider <$> L.createModuleProviderForExistingModule moduleRef
-
--- TODO
-ptrDisposeModuleProvider = undefined
+    LLVM $ ModuleProvider <$> L.createModuleProviderForExistingModule moduleRef
 
 -- What does this return?
 addTypeName :: Module -> String -> Type -> LLVM Int
-addTypeName (Module moduleRef) name (Type typeRef) = LLVM . withCString name $ \cName ->
-    L.addTypeName moduleRef cName typeRef
+addTypeName (Module moduleRef) name (Type typeRef) = 
+    LLVM . withCString name $ \cName -> fromIntegral <$> L.addTypeName moduleRef cName typeRef
+
+deleteTypeName :: Module -> String -> LLVM ()
+deleteTypeName (Module moduleRef) name = LLVM . withCString name $ L.deleteTypeName moduleRef
+
+getTypeKind :: Type -> LLVM L.TypeKind
+getTypeKind (Type typeRef) = LLVM $ L.getTypeKind typeRef
+
+int1Type :: Type
+int1Type = Type L.int1Type
+
+int8Type :: Type
+int8Type = Type L.int8Type
+
+int16Type :: Type
+int16Type = Type L.int16Type
+
+int32Type :: Type
+int32Type = Type L.int32Type
+
+integerType :: Int -- ^ Width in bits
+               -> Type
+integerType = Type . L.integerType . fromIntegral
+
+getIntTypeWidth :: Type -> LLVM Int
+getIntTypeWidth (Type typeRef) = LLVM $ fromIntegral <$> L.getIntTypeWidth typeRef
